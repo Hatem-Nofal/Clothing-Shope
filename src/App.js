0e9bb4cd2591bcfea236b1a10c3e7c4acc1efea7
Hatem Nofal <eng.hatemnofal@gmail.com>
2019-08-28 17:11:53 +0200

    HomePageComponent

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HomePAge from "./HomePage/HomePage.component";
+function App() {
+  return (
+    <div>
+      <HomePAge></HomePAge>
+    </div>
+  );
+}
+
+export default App;
