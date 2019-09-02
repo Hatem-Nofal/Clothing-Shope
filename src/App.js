1a680e182da68710b9612158b998f0d13a2bdf8e
Hatem Nofal <eng.hatemnofal@gmai.com>
2019-09-02 02:16:00 +0200

    collections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.component";
+import ShopPage from "./pages/shop/shop.component";
+import "./App.css";
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
